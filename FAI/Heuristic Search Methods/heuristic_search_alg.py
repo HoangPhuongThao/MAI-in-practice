9a744520a62537132338a068b9e8890dfbc9f43f
didierquintius <71985682+didierquintius@users.noreply.github.com>
2020-10-25 10:24:51 -0300

    Network Heuristic

--- FAI/Heuristic Search Methods/heuristic_search_alg.py
+++ FAI/Heuristic Search Methods/heuristic_search_alg.py
@@ -1,5 +1,23 @@
-from search_algorithms import detect_loop
 
+
+from numpy import inf
+import sys
+
+def detect_loop(path):
+    # check if path only contains unique values by comparing the length of the
+    # path with the path of the unique values of the path
+    return len(path) > len(set(path))
+
+
+def check_max_size_queue(max_size_queue, queue):
+    # update memory
+    queue_size = sys.getsizeof(queue)
+    
+    # edit max size of the queue if the size of the queue increased
+    if queue_size > max_size_queue: 
+       max_size_queue = queue_size 
+       
+    return max_size_queue
 
 def sortChildrenByHeuristicVal(children, heuristicValueList):
     # sort children in an ascending order by their heuristic value
@@ -104,3 +122,94 @@ def beamSearch(goal, network, heuristicValueList, width=2):
 
     return [], max_size_queue
 
+def HillClimbing2(goal, network):
+    '''
+    
+    A basic search algorithm, where the queue starts with the start node as the
+    only path in the queue. Next for each itereation the first path in removed 
+    and all of the children of this path are created. Further, all the new paths
+    that contain loops are removed. Then the new paths are added to the back of 
+    the queue. The network stop as soon as there is a connection to the goal node.
+    The function returns the path to the goal and largest size the queue was 
+    during the iterations.
+
+    '''
+    
+    # initialize the queue and the max size of the queue
+    queue = [0]
+    max_size_queue = 1
+
+    # loop until the queue is empty
+    while queue:        
+        
+        # create list with children of the path in the queue
+        new_paths = [queue + [node] for node in network.return_connections(queue[-1])]        
+        
+        # remove new_paths with loops
+        new_paths = [path for path in new_paths if not detect_loop(path)]
+        
+        if not new_paths: 
+            print("No path found")
+            return [], sys.getsizeof(queue)
+        
+        min_heuristic_value = inf
+        
+        # store the path with the lowest heuristic
+        for path in new_paths:
+            if network.get_heuristic(path[-1]) < min_heuristic_value:
+                queue = path
+                min_heuristic_value = network.get_heuristic(path[-1])        
+
+        if queue[-1] == goal: return queue, sys.getsizeof(queue)
+            
+    return [], max_size_queue
+
+def Greedy(goal, network):
+    '''
+    
+    A basic search algorithm, where the queue starts with the start node as the
+    only path in the queue. Next for each iteration the first path is removed
+    and all of the children of this path are created. Further, all the new paths
+    that contain loops are removed. Then the new paths are added to the front of 
+    the queue. The algorithm stops as soon as there is a connection to the goal node.
+    The function returns the path to the goal and the largest size the queue was
+    during the iterations (i.e. the memory).
+
+    '''
+    
+    # initialize the queue and the max size of the queue
+    queue = [[0]]
+    max_size_queue = sys.getsizeof(queue)
+
+    # loop until the queue is empty
+    while queue:
+        
+        # remove the path in the front of the queue
+        front = queue.pop(0)
+        
+        # create list with children of the path that was in front of the queue
+        new_paths = [front + [node] for node in network.return_connections(front[-1])]        
+        
+        # remove new_paths with loops
+        new_paths = [path for path in new_paths if not detect_loop(path)]
+        
+        # check if new paths reach the goal 
+        for path in new_paths: 
+            if path[-1] == goal: return path, max_size_queue
+            
+        # add new paths to the front of the queue
+        queue = new_paths + queue
+        
+        # calculate the heursitic of each path
+        heuristic_values = [network.get_heuristic(path[-1]) for path in queue]
+        
+        # save a list with sorted heuristics of each path
+        heuristic_values_sorted = heuristic_values.copy()
+        heuristic_values_sorted.sort()
+        
+        # use index of sorted heuristics list to sort the queue
+        queue = [queue[heuristic_values.index(heuristic)] for heuristic in heuristic_values_sorted]
+                
+        max_size_queue = check_max_size_queue(max_size_queue, queue)
+    
+    return [], max_size_queue
---
+++ FAI/Heuristic Search Methods/run.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  4 11:44:09 2020
+
+@author: didie & thao
+"""
+import pandas as pd
+import numpy as np
+from time import time
+from tqdm import tqdm
+import seaborn as sns
+import matplotlib.pyplot as plt
+from network_heuristic import index_heuristic_network
+from heuristic_search_alg import HillClimbing2, Greedy
+#%%
+
+# initialize dataframe with results
+test_results = pd.DataFrame([], columns = ["Algorithm","Number of nodes in network","Branching Factor", "Average Speed", "Average Memory Usage", "Average Length Goal Path"])
+
+# select value for the amount of nodes and probabilities of the connectivity
+network_nodes = [50, 100] #, 750, 1000, 1500]
+branching_factors = [2] #, 3, 5, 7, 10]
+
+amount_of_test_runs = 3
+algorithms = {"HillClimbing2": HillClimbing2, "Greedy" : Greedy}
+
+for algorithm_name, algorithm in algorithms.items():
+    for nodes in network_nodes:
+        for branching_factor in branching_factors:
+            # calculate goal node
+            goal = nodes - 1
+            
+            # initialize test variables
+            speed = np.zeros(amount_of_test_runs)
+            memory_usage = np.zeros(amount_of_test_runs)
+            length_goal_path = np.zeros(amount_of_test_runs)
+
+            print("\n", algorithm_name, nodes, branching_factor)
+            
+            for i in tqdm(range(amount_of_test_runs)):
+                network = index_heuristic_network(nodes, branching_factor, seed = i)
+                start = time()
+                goal_path, memory_usage[i] = algorithm(goal, network)
+                speed[i]= time() - start
+                length_goal_path[i] = len(goal_path)
+            
+            # add averaged test values to the data frame
+            test_results.loc[len(test_results)] = [algorithm_name,nodes,branching_factor,
+                                                   speed.mean(), memory_usage.mean(),
+                                                   length_goal_path.mean()]
+#%% Plot Heatmaps
+
+# select the parameters for the x and y variable
+x_y_axes = [["Algorithm","Number of nodes in network"],["Algorithm","Branching Factor"]]
+
+# select z variables for heatmap
+variables = ["Average Speed", "Average Memory Usage", "Average Length Goal Path"]
+
+# iterate over all possible combinations
+for x_y_ax in x_y_axes:
+    # group data by relevant x and y parameters
+    data = test_results.groupby(by = x_y_ax).mean()
+    for variable in variables:
+        data_var = data[[variable]].unstack(level = 0)
+        data_var.columns = data_var.columns.droplevel()    
+        plt.figure()
+        sns.heatmap(data_var, cmap = "Reds").set_title(variable)
+        plt.savefig('Evaluation/' + variable + '_' + x_y_ax[1] + '.png')
