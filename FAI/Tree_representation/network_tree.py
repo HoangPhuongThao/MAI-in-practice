f2d0411e812fd47f185776539409d45f20fca5b7
Hoang Phuong Thao <thaohanicka@seznam.cz>
2020-10-29 19:55:22 +0100

    Visualisation of a network tree

--- FAI/Tree_representation/network_tree.py
+++ FAI/Tree_representation/network_tree.py
@@ -3,6 +3,7 @@ import os
 PATH, TAIL = os.path.split(os.path.abspath(os.getcwd()))
 sys.path.insert(1, PATH + '/Basic Search Algorithms')
 from network import Network
+from draw_tree import drawTree2
 
 
 class Node(object):
@@ -16,8 +17,8 @@ class Node(object):
         self.children.append(child)
 
     def print_nodefamily(self):
-        prefix = " " * 3 * self.get_level()
-        prefix += "|--" if self.parent else "--"
+        prefix = " " * 4 * self.get_level()
+        prefix += "└──>" if self.parent else "──>"
         print(prefix, str(self.data))
         if self.children:
             for child in self.children:
@@ -39,7 +40,8 @@ class NetworkTree(object):
     def build_tree(self, current_node):
         if current_node.data == 0:
             self.root = current_node
-        children_val = [val for val in network.return_connections(current_node.data) if val > current_node.data]
+        children_val = [val for val in network.return_connections(current_node.data)
+                        if not self.is_loop(val, current_node)]
         for child_val in children_val:
             child = Node(child_val)
             current_node.add_child(child)
@@ -48,14 +50,27 @@ class NetworkTree(object):
     def print_tree(self):
         self.root.print_nodefamily()
 
+    @staticmethod
+    def is_loop(child_val, parent_node):
+        loop = False
+        while not loop and parent_node is not None:
+            if parent_node.data == child_val:
+                loop = True
+            else:
+                parent_node = parent_node.parent
+        return loop
 
+
+# test the visualisation of the arbitrary network tree
 if __name__ == "__main__":
-    amountOfNodes = 10
+    # define the attributes of the tree
+    amountOfNodes = 5
     branchingFactor = 2
     goal = amountOfNodes-1
     network = Network(amountOfNodes, branchingFactor, seed=0, secure_path_to_goal=True)
     print(network.cost_matrix)
 
+    # build the tree from a root
     root = Node(0)
     networkTree = NetworkTree()
     networkTree.build_tree(root)
